513e13d5dc1e08e06736deee3d4b65c7ca2141c9
Sanket Achari <sanketachari@ufl.edu>
2017-10-01 14:13:15 -0400

    Removed earlier prof login, corrected existing test cases

--- src/app/professor/professor.component.spec.ts
+++ src/app/professor/professor.component.spec.ts
@@ -1,20 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProfessorComponent } from './professor.component';
+import { Proflogin2Component } from './proflogin2.component';
+import {MdTabsModule, MdCardModule, MdTooltipModule, MdIconModule, MdMenuModule} from "@angular/material";
+import {DndModule, DragDropService, DragDropConfig} from "ng2-dnd";
+import {DataService} from "../data.service";
+import {StarRatingModule} from "angular-star-rating";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {HttpClientModule} from "@angular/common/http";
 
-describe('ProfessorComponent', () => {
-  let component: ProfessorComponent;
-  let fixture: ComponentFixture<ProfessorComponent>;
+describe('Proflogin2Component', () => {
+  let component: Proflogin2Component;
+  let fixture: ComponentFixture<Proflogin2Component>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfessorComponent ]
+      imports: [MdTabsModule, MdCardModule, DndModule.forRoot(), StarRatingModule.forRoot(), MdTooltipModule,
+                MdIconModule, MdMenuModule, FormsModule, HttpModule, HttpClientModule],
+      declarations: [ Proflogin2Component ],
+      providers: [DragDropService, DragDropConfig, DataService]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProfessorComponent);
+    fixture = TestBed.createComponent(Proflogin2Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/professor/professor.component.ts
+++ src/app/professor/professor.component.ts
@@ -1,70 +1,94 @@
-import {Component, OnInit} from '@angular/core';
-import {DataService} from '../data.service';
-import {Angular2Csv} from 'angular2-csv/Angular2-csv';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import {Angular2Csv} from "angular2-csv";
+import {DataService} from "../data.service";
 import * as _ from 'underscore';
+import 'rxjs/add/operator/takeUntil';
+import {Subject, Subscription} from "rxjs";
 
 @Component({
-    selector: 'app-professor',
-    templateUrl: './professor.component.html',
-    styleUrls: ['./professor.component.scss'],
+  selector: 'app-proflogin2',
+  templateUrl: './proflogin2.component.html',
+  styleUrls: ['./proflogin2.component.scss']
 })
-export class ProfessorComponent implements OnInit {
+export class Proflogin2Component implements OnInit, OnDestroy {
 
-    droppedItems = [];
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
 
-    courses = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []};
-    courseKeys = Object.keys(this.courses);
+  droppedItems = [];
+  courses = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []};
 
-    students: any;
-    profCourses: any;
+  students: any;
+  studentSubscription: Subscription;
+  coursesSubscription:Subscription;
+  search:string;
+  profCourses: any;
 
-    constructor(private dataService: DataService) {
-        dataService.getStudents().subscribe(x => {
-            this.students = x
-        });
-        dataService.getProfCourses().subscribe(x => {
-            this.profCourses = x
-        });
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit() {
+
+    this.dataService.getStudents()
+        .takeUntil(this.ngUnsubscribe)
+        .subscribe(x => { this.students = x; });
+
+    this.dataService.getProfCourses()
+        .takeUntil(this.ngUnsubscribe)
+        .subscribe(x => { this.profCourses = x; });
+  }
+
+  ngOnDestroy(){
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+    /*this.studentSubscription.unsubscribe();
+    this.coursesSubscription.unsubscribe();*/
+  }
+
+  onItemDrop(event: any, courseNo: number) {
+    this.droppedItems.push(event.dragData);
+    this.courses[courseNo].push(event.dragData);
+    return event.dragData;
+  }
+
+  getStudents(){  
+
+    if(_.isEmpty(this.search)) 
+      return this.students; 
+
+    return _.chain(this.students) 
+        .filter(student =>  student.name.toLowerCase().startsWith(this.search.toLowerCase())) 
+        .value(); 
+  }
+
+  getColor(student: any) {
+    if (this.droppedItems.indexOf(student) > -1) {
+      return '#757575';
     }
+    return 'white';
+  }
 
-    ngOnInit() {
+  isAvailable(student: any) {
+    if (this.droppedItems.indexOf(student) > -1) {
+      // console.log(student.name + ": " + "false");
+      return false;
     }
+    return true;
+  }
 
-    onItemDrop(event: any, courseNo: number) {
-        this.droppedItems.push(event.dragData);
-        this.courses[courseNo].push(event.dragData);
-        return event.dragData;
+  removeTA(student: any, courseNo: number) {
+    this.courses[courseNo].splice(this.courses[courseNo].indexOf(student), 1);
+    this.droppedItems.splice(this.droppedItems.indexOf(student), 1);
+  }
+
+  getTAs(courseNo: number) {
+    return this.courses[courseNo];
+  }
+
+  exportToCSV(courseNo: number) {
+
+    if (!_.isEmpty(this.courses[courseNo])) {
+      return new Angular2Csv(this.courses[courseNo], this.profCourses[courseNo], {headers: Object.keys(this.courses[courseNo][0])});
     }
+  }
 
-    getColor(student: any) {
-        if (this.droppedItems.indexOf(student) > -1) {
-            return '#757575';
-        }
-        return 'white';
-    }
-
-    isAvailable(student: any) {
-        if (this.droppedItems.indexOf(student) > -1) {
-            // console.log(student.name + ": " + "false");
-            return false;
-        }
-        return true;
-    }
-
-    removeTA(student: any, courseNo: number) {
-        this.courses[courseNo].splice(this.courses[courseNo].indexOf(student), 1);
-        this.droppedItems.splice(this.droppedItems.indexOf(student), 1)
-    }
-
-    getTAs(courseNo: number) {
-        return this.courses[courseNo];
-    }
-
-    exportToCSV(courseNo: number) {
-
-        if (!_.isEmpty(this.courses[courseNo])) {
-            console.log(JSON.stringify(this.courses[courseNo]));
-            return new Angular2Csv(this.courses[courseNo], this.profCourses[courseNo], {headers: Object.keys(this.courses[courseNo][0])});
-        }
-    }
 }
