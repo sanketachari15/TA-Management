219ecb3dd41036bf7e8c8cbafb3d56f682fd9821
Sanket Achari <sanketachari@ufl.edu>
2017-10-08 03:14:46 -0400

    e2e test cases

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,7 +6,43 @@ export class AppPage {
         return browser.get(path);
     }
 
+    getTitle(){
+        return browser.getTitle();
+    }
+
+    getHeader(){
+        return element(by.css('.header-name')).getText();
+    }
+
     getLoginPageDetails(buttonClass) {
         return element(by.css(buttonClass)).getText();
+    }
+
+    getProfPage(){
+        return element(by.css('.middle')).isPresent();
+    }
+
+    /*getCoursesList(){
+        return element(by.css('.prof-courses'));
+    }*/
+
+    getProfPageSearchInput(){
+        return element(by.model('search'));
+    }
+
+
+    getProfPageStudentList(){
+        let students = element(by.repeater('let student of getStudents()'));
+        return students.element(by.binding('student.name'))
+    }
+
+    getProfPageStudentCard(){
+        return element.all(by.css('.student-details'))
+    }
+
+    dragAndDropStudent(){
+        let student = element(by.css('.student-details'));
+        let dashborad = element(by.css('.student-details'));
+        // browser.actions().dragAndDrop().perform();
     }
 }
