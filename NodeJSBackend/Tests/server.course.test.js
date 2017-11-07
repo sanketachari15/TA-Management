17a7b8014611c0ed8c0b545a4cc948cc0cc0c71c
sanketachari15 <31542330+sanketachari15@users.noreply.github.com>
2017-11-07 10:46:48 -0500

    Revert "Update and delete api test cases along with code coverage script"

--- NodeJSBackend/Tests/server.course.test.js
+++ NodeJSBackend/Tests/server.course.test.js
@@ -13,6 +13,7 @@ const testCourses = [{
   ProfessorFullName: 'TestProf1',
   ProfessorEmail: 'TestProf1TestProf1@ufl.edu',
   CourseScheduleLink: 'courseScheduleTest1',
+  CourseSyllabusLink: 'courseSyllabusTest1',
   MaxStudents: 50,
   TAs: ['17162351']
 }, {
@@ -24,6 +25,7 @@ const testCourses = [{
   ProfessorFullName: 'TestProf2',
   ProfessorEmail: 'TestProf2TestProf2@ufl.edu',
   CourseScheduleLink: 'courseScheduleTest2',
+  CourseSyllabusLink: 'courseSyllabusTest2',
   MaxStudents: 50,
   TAs: ['17162351']
 }];
@@ -122,51 +124,6 @@ describe('Get courses/Code', () => {
     request(app)
     .get(`/courses/${falseCourseCode}`)
     .expect(404)
-    .end(done);
-  });
-});
-
-describe('Delete courses/Code', () => {
-  it('should remove a course', (done) => {
-    var courseCode = testCourses[0].Code;
-    request(app)
-    .delete(`/courses/${courseCode}`)
-    .expect(200)
-    .expect((response) => {
-      expect(response.body.course.Code).toBe(courseCode)
-    })
-    .end((error, response) => {
-      if(error){
-        return done(error);
-      }
-      Course.find({Code: courseCode}).then((courses) => {
-        expect(courses.length).toBe(0);
-        done();
-      }).catch((error) => done(error));
-    });
-  });
-
-  it('should return 404 if course not found', (done) => {
-    var falseCourseCode = "invalidCourseCode"
-    request(app)
-    .delete(`/courses/${falseCourseCode}`)
-    .expect(404)
-    .end(done);
-  });
-});
-
-describe('Update courses/Code', () => {
-  it('should update the course', (done) => {
-    var courseCode = testCourses[0].Code;
-    request(app)
-    .patch(`/courses/${courseCode}`)
-    .send({"ProfessorFullName": "TestProf3", "MaxStudents": 100})
-    .expect(200)
-    .expect((response) => {
-      expect(response.body.course.ProfessorFullName).toBe('TestProf3');
-      expect(response.body.course.MaxStudents).toBeA('number');
-      expect(response.body.course.MaxStudents).toBe(100);
-    })
     .end(done);
   });
 });
