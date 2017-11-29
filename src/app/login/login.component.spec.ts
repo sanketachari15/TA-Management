bbfca90278d9654a685cf1cfb5963ee3211f2387
Prem Ankur <premacck1@ufl.edu>
2017-11-29 12:50:46 -0500

    Unit test case for new Login Dialog included

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -5,6 +5,9 @@ import {RouterTestingModule} from "@angular/router/testing";
 import {MdToolbarModule} from "@angular/material";
 import {SharedService} from "../shared.service";
 import { CarouselModule } from 'angular4-carousel';
+import { loginPopComponent } from './login.component';
+import {MdDialogModule, MdDialogRef, MD_DIALOG_DATA, MdDialog} from '@angular/material';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -12,9 +15,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
-      imports: [RouterTestingModule, MdToolbarModule, CarouselModule],
-      providers: [SharedService]
+      declarations: [ LoginComponent, loginPopComponent ],
+      imports: [RouterTestingModule, MdToolbarModule, CarouselModule, MdDialogModule],
+      providers: [SharedService, MdDialogModule]
     })
     .compileComponents();
   }));
