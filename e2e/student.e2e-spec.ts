ea5c45db333e697dfdc74a36fcabf5c08974cabf
Prem Ankur <premankur19@gmail.com>
2017-11-29 20:10:34 -0500

    Adding more e2e test cases for student profile.

--- e2e/student.e2e-spec.ts
+++ e2e/student.e2e-spec.ts
@@ -5,6 +5,7 @@ import elementLocated = until.elementLocated;
 describe('Student Page', () => {
 
     let EC = protractor.ExpectedConditions;
+    let path = require('path');
 
     beforeEach(() => {
         browser.get('/login');
@@ -29,24 +30,48 @@ describe('Student Page', () => {
         expect(element(by.id('profilePage')).isPresent()).toBe(true);
     });
 
-    it('student should be able to change gpa', () => {
+    it('student should be able to change gpa in the profile', () => {
         element(by.linkText('Profile')).click();
         element(by.id('changeGpa')).click();
+        browser.sleep(100);
         const gpa = element(by.id('enterGpa'));
         gpa.sendKeys('\b\b\b\b3.9');
         element(by.id('okButton')).click();
-        browser.sleep(500);
+        browser.sleep(600);
         expect(element(by.id('gpa')).getText()).toEqual('GPA: 3.9');
     });
 
-    // it('student should be able to upload profile picture', () => {
-    //     element(by.linkText('Profile')).click();
-    //     expect(element(by.id('profilePage')).isPresent()).toBe(true);
-    // });
+    it('student should be able to upload profile picture', () => {
+        element(by.linkText('Profile')).click();
+        const fileToUpload = '../src/assets/images/ta2.jpg',
+        absolutePath = path.resolve(__dirname, fileToUpload);
+        element(by.id('pictureUpload')).sendKeys(absolutePath);
+        browser.sleep(2000);
+        expect(element(by.id('picture')).isPresent()).toBe(true);
+    });
+
+    it('Student should be able to view his Resume', () => {
+        element(by.linkText('Profile')).click();
+        element(by.buttonText('View Resume')).click();
+        browser.sleep(2500);
+        expect(element(by.id('taResume')).isDisplayed()).toBe(true);
+    });
 
     it('should display student home on selection', () => {
         element(by.linkText('Home')).click();
         expect(element(by.id('homePage')).isPresent()).toBe(true);
     });
 
+    it('Student should be able to see his Recent Announcements, Messages and Files', () => {
+        element(by.linkText('Home')).click();
+        element(by.linkText('Home')).click();
+        browser.sleep(500);
+        element(by.linkText('Announcements')).click();
+        browser.sleep(500);
+        element(by.linkText('Messages')).click();
+        browser.sleep(500);
+        element(by.linkText('Files')).click();
+        browser.sleep(500);
+        expect(element(by.linkText('Files')).isPresent()).toBe(true);
+    });
 });
