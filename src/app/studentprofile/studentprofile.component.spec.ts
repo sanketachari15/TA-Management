f499211891bc44eeddfa3049589c6505d9b994c7
Prem Ankur <premankur19@gmail.com>
2017-11-30 14:34:32 -0500

    Added unit more test cases for Student. Total 30 Unit test Cases. All running.

--- src/app/studentprofile/studentprofile.component.spec.ts
+++ src/app/studentprofile/studentprofile.component.spec.ts
@@ -20,6 +20,19 @@ import { StudenthomeComponent } from '../studenthome/studenthome.component';
 import { StudentComponent } from '../student/student.component';
 
 describe('StudentprofileComponent', () => {
+
+  const studentHome = [
+    {FullName: 'John Johansson', Sem: 'Fall2017', Email: 'johnjohansson@ufl.edu', Courses: [{name: "COP5615: Distributed Operating Systems",
+      announcements: ["Professor has requested a meeting", "Professor is not taking office hours this week", 
+      "Grades are now released for assignment 2"],
+      messages: [{from: "Professor X",message: "Please send me the list of students whose presentations are due friday."},
+                 {from: "Student 1", message: "Can i please get an extention on assignment 3?"},
+                 {from: "Student 2", message: "I need to request a meeting with you during TA hours tommorow."}]
+      }
+    ]}];
+    const students = [
+      {UFID: 123456789011, FirstName: "John", LastName: "Johansson", gpa: "3.6", resumeLink: "../../assets/pdf/resum.pdf"}];
+
   let component: StudentprofileComponent;
   let fixture: ComponentFixture<StudentprofileComponent>;
   const resumeLink = '../../assets/pdf/resum.pdf';
@@ -38,6 +51,8 @@ describe('StudentprofileComponent', () => {
     fixture = TestBed.createComponent(StudentprofileComponent);
     component = fixture.componentInstance;
     component.resumeLink = resumeLink;
+    component.students = students;
+    component.student = students[0];
     fixture.detectChanges();
   });
 
@@ -48,27 +63,23 @@ describe('StudentprofileComponent', () => {
   it('should show the student resume', () => {
     expect(component).toBeTruthy();
   });
+
+it('Student list should not be empty', async(() => {
+    expect(component.students).toBeTruthy();
+    expect(!((component.students[0].LastName).toBeNull));
+    expect(!((component.students[0].LastName).toBeUndefined));
+    expect((component.students[0].LastName).toBeTruthy);
+  }));
+
+it('Logged in student should be selected with same id', () => {
+  expect(component.student).toBeTruthy();
+  expect(component.student.UFID).toBe(123456789011);
+  expect(component.student.FirstName).toBe('John');
+  expect(component.student.LastName).toBe('Johansson');
 });
 
-// it('Student list should not be empty', async(() => {
-//   let dataService = fixture.debugElement.injector.get(DataService);
-//   let spy = spyOn(dataService, 'getStudents')
-//   .and.returnValue(Promise.resolve(testData));
-//   fixture.detectChanges();
-//   fixture.whenStable().then(() => {
-//     expect(component.students).toBeTruthy();
-//     expect(component.students.length > 0).toBe(true);
-//   });
-// }));
-
-// it('Logged in student should be selected with same id', () => {
-//   expect(component.student).toBeTruthy();
-//   expect(component.student.UFID).toBe('123456789011');
-//   expect(component.student.FirstName).toBe('John');
-//   expect(component.student.LastName).toBe('Johansson');
-// });
-
-// it('Student should have a resume', () => {
-//   expect(component.student).toBeTruthy();
-//   expect(component.student.resumeLink === '').toBe(false);
-// });
+it('Student should have a resume', () => {
+  expect(component.student).toBeTruthy();
+  expect(component.student.resumeLink === '').toBe(false);
+});
+});
